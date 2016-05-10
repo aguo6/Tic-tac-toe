f3f0bc9467035086dd80f23188fad7aecdfa5d72
Eric <ewang7@students.d125.org>
2016-05-10 09:20:13 -0500

    playerPiece to arrayBoard & appletBoard created

---
+++ appletBoard.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class appletBoard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class appletBoard
+{
+    public void paint(Graphics g)
+    {
+        
+    }
+}
--- arrayBoard.java
+++ arrayBoard.java
@@ -12,8 +12,14 @@ public class arrayBoard
         Space [][] arrayBoard = new Space [3][3];
     }
     
-    public void placePiece()
+    public void placePiece(Space clickedSpace, int playerNum)
     {
-        
+        if ((!clickedSpace.isBlank()) && (clickedSpace.getPlayer() != 0))
+        {
+            clickedSpace.setPlayer(playerNum);
+            clickedSpace.setBlank(false);
+        }
     }
+    
+    
 }
