e8aaab9a7a51e0772ba2a6ec259839aca10f176e
Eric <ewang7@students.d125.org>
2016-05-10 08:54:37 -0500

    created arrayBoard

---
+++ arrayBoard.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class arrayBoard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class arrayBoard
+{
+    public arrayBoard()
+    {
+        Player [][] arrayBoard = new Player [3][3];
+    }
+    
+}
