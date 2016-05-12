11f8e02216d9772eb7be85131b98c1996719333c
Eric <ewang7@students.d125.org>
2016-05-12 08:21:37 -0500

    Added lines for game board

--- appletBoard.java
+++ appletBoard.java
@@ -10,6 +10,23 @@ public class appletBoard
 {
     public void paint(Graphics g)
     {
+        // 4-lines of tic-tac-toe
         g.drawLine(166,0,166,500);
+        g.drawLine(332,0,332,500);
+        g.drawLine(0,166,498,166);
+        g.drawLine(0,332,498,332);
+        
+        
+        
     }
+    
+    
+    
+    
+    public void playerPieces (Graphics g)
+    {
+        
+    }
+    
+    
 }
