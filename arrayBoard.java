030d82939185170cc300b35a33775a74bb707899
aguo6 <aguo6@students.d125.org>
2016-05-19 08:33:29 -0500

    Update arrayBoard.java
    re-add checkwin

--- arrayBoard.java
+++ arrayBoard.java
@@ -22,4 +22,41 @@ public class arrayBoard
     }
     
     
+    +     public boolean checkWin ()
+ +    {
+ +        boolean win = false;
+ +
+ +        // check rows and columns
+ +        for (int i=0; i<3; i++) {
+ +            if ((arrayBoard[i][1].getPlayer()).equals(arrayBoard[i][0].getPlayer())) {
+ +                if ((arrayBoard [i][2].getPlayer()).equals(arrayBoard[i][0].getPlayer())){
+ +                    win = true;
+ +                }
+ +            }
+ +        }
+ +
+ +        for (int j=0; j<3; j++) {
+ +            if ((arrayBoard[1][j].getPlayer()).equals(arrayBoard[0][j].getPlayer())) {
+ +                if ((arrayBoard [2][j].getPlayer()).equals(arrayBoard[0][j].getPlayer())){
+ +                    win = true;
+ +                }
+ +            }
+ +        }
+ +
+ +        //check diag
+ +        if ((arrayBoard[1][1].getPlayer()).equals(arrayBoard[0][0].getPlayer())) {
+ +            if ((arrayBoard [1][1].getPlayer()).equals(arrayBoard[2][2].getPlayer())){
+ +                win = true;
+ +            }
+ +        }
+ +        if ((arrayBoard[0][2].getPlayer()).equals(arrayBoard[1][1].getPlayer())) {
+ +            if ((arrayBoard [2][0].getPlayer()).equals(arrayBoard[1][1].getPlayer())){
+ +                win = true;
+ +            }
+ +        }
+ +        return win;
+ +    }
+ +    
+    
+    
 }
